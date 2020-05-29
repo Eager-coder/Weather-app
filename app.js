1f086032249526fea05cd8cec48a47185954a339
Turan Sultan <turan.sultann@gmail.com>
2020-05-29 22:04:17 +0600

    commit

--- app.js
+++ app.js
@@ -65,6 +65,7 @@ function renderDate(){
     const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     date.innerText = dayNames[currentDate.getDay()] + ' ' + (currentDate.getDay() + 1) + ' ' + monthNames[currentDate.getMonth()];
 };
+
 function setIcons(desc){
     iconArray = ['01d', '01n', '02d', '02n', '03d', '03n', '04d', '04n', '09d', '09n', '10d', '10n', '11d', '11n', '13d', '13n', '50d', '50n'];
     for (icon of iconArray){
